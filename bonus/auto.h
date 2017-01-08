ae6eed9a8ee4c9ead25cc018140b637b25fa062b
Antoine Baché <antoine.bache@epitech.eu>
2017-01-08 05:00:56 +0100

    Implemented auto type

---
+++ bonus/auto.h
@@ -0,0 +1,16 @@
+/*
+** auto.h for Cpp_SKL in /home/bache_a/work/piscine_cpp/cpp_SKL/bonus
+**
+** Made by Antoine Baché
+** Login   <bache_a@epitech.net>
+**
+** Started on  Sun Jan  8 04:59:34 2017 Antoine Baché
+** Last update Sun Jan  8 05:00:46 2017 Antoine Baché
+*/
+
+#ifndef AUTO_H_
+# define AUTO_H_
+
+#define auto	__auto_type
+
+#endif /* !AUTO_H_ */
