23e2c50d343b59b2d6ec5e099ddc4b1e0e801973
Antoine Baché <antoine.bache@epitech.eu>
2017-01-08 05:41:48 +0100

    Added types.h

---
+++ bonus/types.h
@@ -0,0 +1,53 @@
+/*
+** types.h for CppPool in /home/bache_a/work/piscine_cpp/cpp_SKL/bonus
+**
+** Made by Antoine Baché
+** Login   <bache_a@epitech.net>
+**
+** Started on  Sun Jan  8 05:36:15 2017 Antoine Baché
+** Last update Sun Jan  8 05:41:26 2017 Antoine Baché
+*/
+
+#ifndef TYPES_H_
+# define TYPES_H_
+
+/*
+** Misc
+*/
+# include "raise.h"
+# include "auto.h"
+# include "exceptions.h"
+# include "new.h"
+# include "smart_ptr.h"
+
+/*
+** Containers
+*/
+# include "array.h"
+# include "list.h"
+
+/*
+** Types
+*/
+# include "bool.h"
+# include "byte.h"
+# include "char.h"
+# include "double.h"
+# include "float.h"
+# include "int.h"
+# include "int8_t.h"
+# include "int16_t.h"
+# include "int32_t.h"
+# include "int64_t.h"
+# include "short.h"
+# include "String.h"
+# include "uchar.h"
+# include "uint.h"
+# include "uint8_t.h"
+# include "uint16_t.h"
+# include "uint32_t.h"
+# include "uint64_t.h"
+# include "ulong.h"
+# include "ushort.h"
+
+#endif /* !TYPES_H_ */
