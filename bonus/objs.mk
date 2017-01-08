6dd6da897714cc863acacd44e97c27031669b69c
Antoine Baché <antoine.bache@epitech.eu>
2017-01-08 03:53:42 +0100

    Added a Makefile

--- bonus/objs.mk
+++ bonus/objs.mk
@@ -5,10 +5,10 @@
 ## Login   <bache_a@epitech.net>
 ##
 ## Started on  Fri Nov 25 20:29:09 2016 Antoine Baché
-## Last update Wed Jan  4 11:38:09 2017 Antoine Baché
+## Last update Sun Jan  8 03:51:10 2017 Antoine Baché
 ##
 
-OBJ_DIR:=		./obj/
+OBJ_DIR:=		./
 
 OBJ_AS:=	$(SRC:%.s=$(OBJ_DIR)%.o)
 OBJ_C:=		$(SRC:%.c=$(OBJ_DIR)%.o)
