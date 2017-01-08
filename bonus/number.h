af74f89ca34175eacf3ede03653e588543709246
Arthur Arnaud <arthur.arnaud@epitech.eu>
2017-01-08 03:19:40 +0000

    fix number.h

--- bonus/number.h
+++ bonus/number.h
@@ -8,7 +8,7 @@
 # include "bool.h"
 # include "object.h"
 
-typedef Object* (*real_op)(const Object* self, const Object* other, bool place);
+typedef Object* (*real_op)(const Object* self, const Object* other);
 typedef bool (*real_cmp)(const Object* self, const Object* other);
 
 typedef struct {
